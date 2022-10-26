fb196bb9ee570e89b92b086178d0f5d53f480aaf
EhsanAli248 <ehsanalia555@gamil.com>
2022-10-26 06:39:10 -0700

    Vue Router Learning

---
+++ router/src/routes.js
@@ -0,0 +1,14 @@
+import {createWebHistory,createRouter} from 'vue-router'
+import HomeComp from './components/HomeComp.vue';
+const routes=[
+    {
+        name:"HomeComp",
+        path:'/',
+        component:HomeComp
+    }
+];
+const router=createRouter({
+    history:createWebHistory,
+    routes
+}) ;
+export default router;
